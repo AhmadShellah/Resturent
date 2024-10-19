8e4cd973648919c88ab18c01b9d497f6930c4332
Ahmad Shellah <ahmad.yaseen.shellah@gmail.com>
2024-10-19 21:09:22 +0300

    Add Repo And Interface

---
+++ Restaurant/Controllers/OrdersController.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Contracts.AllModels.MealsModels;
+using Contracts.InterFacses;
+using Microsoft.AspNetCore.Mvc;
+using Restaurant.ViewModelsOrDtos;
+using Services;
+
+namespace Restaurant.Controllers
+{
+    //ahmad/Meals/Create
+    //ahmad/Meals/Update
+    //ahmad/Meals/Get
+
+    [ApiController]
+    [Route("ahmad/[controller]/[action]")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        public OrdersController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        //private readonly IMealService _mealService;
+
+
+        [HttpPost]
+        public ActionResult CreateFromEndUser(CreateMealsDto inputFromEndUser)
+        {
+           
+        }
+
+      
+
+    }
+}
